b469585566f4caadf4fc39f1db165c8e0c6dc969
Rubens Montanha <gameplaysoriginal@gmail.com>
2022-10-26 15:23:33 -0300

    first commit

---
+++ tp1.vhd
@@ -0,0 +1,26 @@
+--------------------------------------
+-- Biblioteca
+--------------------------------------
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
+
+--------------------------------------
+-- Entidade
+--------------------------------------
+entity tp1 is
+  port( -- <COMPLETAR>
+      );
+end entity; 
+
+--------------------------------------
+-- Arquitetura
+--------------------------------------
+architecture tp1 of tp1 is
+  -- <COMPLETAR>
+
+begin
+    
+  -- <COMPLETAR>
+
+end architecture;
